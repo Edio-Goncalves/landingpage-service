f5672b372ec2922248f01ffd187371e16ed7739c
Edio Goncalves Junior <ediogoncalvesjr@hotmail.com>
2024-10-05 19:05:16 -0300

    Git init

---
+++ src/components/Head.tsx
@@ -0,0 +1,12 @@
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link />
+  <link
+    rel="shortcut icon"
+    href="/assets/image/icon/favicon.svg"
+    type="image/x-icon"
+  />
+  <link rel="stylesheet" href="/assets/style/css/style.css" />
+  <title>Possa & Almeida advogados</title>
+</head>;
